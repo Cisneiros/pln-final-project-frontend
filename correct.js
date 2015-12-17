691b31a4ec8ed655af0bd3f72b9778cdde6c0dc7
Alexandre Cisneiros Filho <alexandre@cisneiros.com>
2015-12-16 22:19:46 -0300

    Metric

--- correct.js
+++ correct.js
@@ -2,17 +2,21 @@
 
 (function(){
   var API_BASE = 'http://127.0.0.1:3500/';
+  //var API_BASE = 'https://pln-final-project-pedrodiniz.c9users.io/';
   var input;
   var suggestionsList;
+  var metrics;
   var positionStart;
   var positionEnd;
+  var points = 0;
+  var uses = 0;
 
   function apiCall(endpoint, payload, cb) {
     var queryString = '?';
     for (var key in payload) {
       queryString += encodeURIComponent(key) + '=' + encodeURIComponent(payload[key]) + '&';
     }
-    queryString = queryString.substr(0, queryString.length-1)
+    queryString = queryString.substr(0, queryString.length-1);
 
     var url = API_BASE + endpoint + queryString;
     var xhr = new XMLHttpRequest();
@@ -34,11 +38,15 @@
 
   function showSuggestions(suggestions) {
     suggestionsList.innerHTML = '';
-    suggestions.forEach(function(suggestion) {
+    suggestions.forEach(function(suggestion, index) {
       var li = document.createElement('li');
       li.innerHTML = suggestion;
       li.addEventListener('click', function() {
         replaceWord(positionStart + 1, positionEnd, suggestion);
+        points += index;
+        uses += 1;
+
+        metrics.innerHTML = 'Desempenho: ' + (points / uses) + ' (quanto menor melhor)';
       });
 
       suggestionsList.appendChild(li);
@@ -57,6 +65,7 @@
   function setup() {
     input = document.querySelector('#input');
     suggestionsList = document.querySelector('#suggestions-list');
+    metrics = document.querySelector('#metrics');
 
     input.addEventListener('keypress', function(ev) {
       if (ev.keyCode == 32) { // 32 = space
